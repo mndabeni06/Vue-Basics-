d4cbb5e954ead2de41ab65822b0ab0cb14e8c880
Masimthembe Ndabeni <mndabeni6@gmail.com>
2021-10-12 13:21:19 +0200

    first commit

--- app.js
+++ app.js
@@ -1,33 +1,29 @@
-const app =  Vue.createApp({
+const app = Vue.createApp({
     data() {
-        return {
-            showBooks: true,
-            title: 'The Final Empire',
-            author: 'Masimthembe Ndabeni',
-            age: '22',
-            x: 0,
-            y: 0
-
-        }
+      return {
+        url: 'https://www.thenetninja.co.uk',
+        showBooks: true,
+        books: [
+          { title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg', isFav: true },
+          { title: 'the way of kings', author: 'brandon sanderson', img: 'assets/2.jpg', isFav: false },
+          { title: 'the final empire', author: 'brandon sanderson', img: 'assets/3.jpg', isFav: true },
+        ],
+      }
     },
     methods: {
-        toggleShowBooks (){
-            this.showBooks = !this.showBooks
-        },
+      toggleShowBooks() {
+        this.showBooks =  !this.showBooks
+      },
 
-        handleEvent(event, data) {
-            console.log(event, event.type)
-            if (data) {
-                console.log(data)
-            }
-        },
-
-        handleMousemove(event) {
-           this.x = event.offsetX
-           this.y = event.offsetY
-        }
+      toggleFav(book){
+        book.isFav  = !book.isFav
+      }
+    },
+    computer : {
+      filteredBooks() {
+        return 
+      }
     }
-
-})
-
-app.mount('#app')
+  })
+  
+  app.mount('#app')
